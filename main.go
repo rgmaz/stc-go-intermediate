83d0cfe743a5fa401e4f6b650ca5b9ede72d2b42
Ryan G. Mazambani <237030736+rgmaz@users.noreply.github.com>
2026-08-19 15:23:15 +0200

    Embedding and Composition

--- main.go
+++ main.go
@@ -5,44 +5,32 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
-type Stack struct {
-	items []int
+type Logger struct{}
+
+func (l Logger) Log(msg string) {
+	fmt.Printf("[log] %s\n", msg)
 }
 
-func (s *Stack) Push(x int) {
-	s.items = append(s.items, x)
+type Counter struct {
+	Logger
+	count int
 }
 
-func (s *Stack) Pop() (int, bool) {
-	if len(s.items) == 0 {
-		return 0, false
-	}
+func (c *Counter) Inc() {
+	c.count++
 
-	lastIndex := len(s.items) - 1
-	popped := s.items[lastIndex]
-	s.items = s.items[:lastIndex]
-
-	return popped, true
+	msg := strconv.Itoa(c.count)
+	c.Log(msg)
 }
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
-	parts := strings.Fields(sc.Text())
-	var s Stack
-	for _, p := range parts {
-		n, _ := strconv.Atoi(p)
-		_ = n
-		s.Push(n)
-	}
-	for {
-		x, ok := s.Pop()
-		if !ok {
-			break
-		}
-		fmt.Println(x)
+	n, _ := strconv.Atoi(sc.Text())
+	var c Counter
+	for range n {
+		c.Inc()
 	}
 }
