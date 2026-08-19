9f1b5baf56c859606637a5792e72e978a3be6a8b
Ryan G. Mazambani <237030736+rgmaz@users.noreply.github.com>
2026-08-19 14:54:24 +0200

    defer, panic, recover

--- main.go
+++ main.go
@@ -5,33 +5,27 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
+
+func safeDivide(a, b int) (q int, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("divide by zero")
+		}
+	}()
+	return a / b, nil
+}
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
-	fields := strings.Fields(sc.Text())
-	a := make(chan int)
-	b := make(chan int)
-	// Producer
-	go func() {
-		defer close(a)
-		for _, f := range fields {
-			n, _ := strconv.Atoi(f)
-			a <- n
-		}
-	}()
-	// Squarer
-	go func() {
-		defer close(b)
-		for n := range a {
-			b <- (n * n)
-		}
-	}()
-	sum := 0
-	for v := range b {
-		sum += v
+	a, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	b, _ := strconv.Atoi(sc.Text())
+	q, err := safeDivide(a, b)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+	} else {
+		fmt.Printf("result: %d\n", q)
 	}
-	fmt.Println(sum)
 }
